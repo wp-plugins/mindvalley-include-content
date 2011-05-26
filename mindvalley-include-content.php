fe486f484ddfb1b3d63a84d1cd706adb4a6f3f91
mindvalley <mindvalley@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-26 07:38:48 +0000

    tagging 1.1
    git-svn-id: https://plugins.svn.wordpress.org/mindvalley-include-content/trunk@389253 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- mindvalley-include-content.php
+++ mindvalley-include-content.php
@@ -4,21 +4,35 @@ Plugin Name: MindValley Include Content
 Plugin URI: http://mindvalley.com
 Description: Creates shortcode [mv_include] to include content from another post/page.
 Author: MindValley
-Version: 1.0
+Version: 1.1
 */
 
 /**
  *  Usage
  *  [mv_include id='<page or post ID>']
+ *  or
+ *  [mv_include slug='<page or post SLUG>']
  *
  */ 
 function mv_includeContentShortcode( $atts ) {
 	$thepostid = (int) $atts['id'];
-
-	if ( !$post = get_post( $thepostid ) )
+	$thepostslug = $atts['slug'];
+	$post  = (!empty($thepostid)) ? get_post( $thepostid ) : get_post_by_slug($thepostslug);
+	if ( !$post )
 	    return '';
 
 	return apply_filters( 'the_content', $post->post_content );
 }
+
+if(!function_exists('get_post_by_slug')):
+function get_post_by_slug($post_name, $output = OBJECT){
+    global $wpdb;
+    $post = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE post_name = %s AND (post_type = 'post' OR post_type='page')", $post_name ));
+    if ( $post )
+        return get_post($post, $output);
+
+    return null;
+}
+endif;
 
 add_shortcode("mv_include", "mv_includeContentShortcode");
